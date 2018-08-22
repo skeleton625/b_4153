5e8c703db3ab64c9dba66863ac5c79b0b8aa93a4
WaniFi <skelpd24@gmail.com>
2018-08-22 15:19:50 +0900

    4153 문제 다른 접근 완료

--- baekjoon_4153/main.cpp
+++ baekjoon_4153/main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-#include <cmath>
-#include <algorithm>
 using namespace std;
 
 int n[3];
 int main() {
 	cin >> n[0] >> n[1] >> n[2];
-	while (n[0] != 0 && n[1] != 0 && n[2] != 0) {
-		sort(n, n + 3);
-		if (pow(n[2], 2) == pow(n[0], 2) + pow(n[1], 2)) cout << "right" << endl;
+	while (n[0]&&n[1]&&n[2]){
+		n[0] *= n[0]; n[1] *= n[1]; n[2] *= n[2];
+		if (n[0]==n[1]+n[2] || n[1]==n[2]+n[0] || n[2]==n[1]+n[0]) cout << "right" << endl;
 		else cout << "wrong" << endl;
 		cin >> n[0] >> n[1] >> n[2];
 	}
